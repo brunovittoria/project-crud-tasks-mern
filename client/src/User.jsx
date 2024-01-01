f7f88793392168b59032eea15c8ee5bca6e321f3
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-01-01 20:00:55 +0100

    Fetch records and display

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 export default function Users(){
 
-    const [users, setUsers] = useState([{
-        Name: "Bruno",
-        Email: "bruno@gmail.com",
-        Age: 24
-    }])
+    const [users, setUsers] = useState([])
+
+    //Here we will fetch the records and display once USERS are ADDED at CreateUser PAGE
+    useEffect(() => {
+        axios.get('http://localhost:3001') //Pegamos a rota principal "/" ao carregar nossa pagina fazemos o fetch do /
+        .then(result => setUsers(result.data))  //Passamos os dados do usuario que criou um novo USER para dentro da useState
+        .catch(err => console.log(err))
+    }, [])
 
     return(
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
@@ -26,12 +30,12 @@ export default function Users(){
                         {
                             users.map((user) => {
                                 return <tr>
-                                    <td>{user.Name}</td>
-                                    <td>{user.Email}</td>
-                                    <td>{user.Age}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.age}</td>
                                     <td>
                                         <Link to="/update" className="btn btn-success">Update</Link>
-                                        <button>Delete</button>
+                                        <button className="btn btn-danger">Delete</button>
                                     </td>
                                 </tr>
                             })
--- server/index.js
+++ server/index.js
@@ -17,9 +17,15 @@ app.use(express.json())
 
 //Routes from our APP
 
-//Create user Route:
+//Home Route
+app.get("/", (req, res) => {
+    UserModel.find({})
+    .then(users => res.json(users)) //Passamos os usuarios dentro do DB pro nosso FRONTEND
+    .catch(err => res.json(err))
+})
 
-app.post("/createUser", (req, res) => {
+//Create user Route:
+app.post("/createuser", (req, res) => {
     UserModel.create(req.body) //Fazemos a requisicao do BODY na do nosso modelo de user do DB
     .then(users => res.json(users)) //Enviamos para nosso frontend a resposta em JSON
     .catch(err => res.json(err))
